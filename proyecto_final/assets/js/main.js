794ff7eb3fb540d3cb83f7dfb5b8e089f896b6a0
luciadifazio <difazio.lucia@gmail.com>
2021-10-31 17:58:07 -0300

    Desafío tres

--- proyecto_final/assets/js/main.js
+++ proyecto_final/assets/js/main.js
@@ -2,7 +2,7 @@ const nombre = prompt("Ingrese su nombre");
 
 const mensaje = "Bienvenido " + nombre + " a El Remanso";
 
-alert (mensaje);
+alert(mensaje);
 console.log(mensaje);
 
 
@@ -11,11 +11,17 @@ let habitacion = prompt("qué habitación preferís?");
 let simple = 2000
 let doble = 3500
 
-if((habitacion =="simple") || (habitacion == "Simple")) {
+if ((habitacion == "simple") || (habitacion == "Simple")) {
     console.log("La habitacion sale 2000");
-} else if((habitacion =="doble") || (habitacion == "Doble")) {
+} else if ((habitacion == "doble") || (habitacion == "Doble")) {
     console.log("La habitacion sale 3500");
-} 
-else{
+} else {
     console.log("datos incorrectos");
 }
+
+
+let huesped;
+for (let huesped = 1; huesped <=2; huesped++){
+    reserva= prompt ("ingrese nombre del huesped");
+}
+    alert("Datos ingresados correctamente");
