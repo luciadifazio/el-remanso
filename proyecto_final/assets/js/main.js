807439c8346ab70dea4ace32bb485ae633d6a6e5
luciadifazio <difazio.lucia@gmail.com>
2021-11-04 16:18:09 -0300

    Desafío Funciones

--- proyecto_final/assets/js/main.js
+++ proyecto_final/assets/js/main.js
@@ -1,4 +1,4 @@
-const nombre = prompt("Ingrese su nombre");
+/*const nombre = prompt("Ingrese su nombre");
 
 const mensaje = "Bienvenido " + nombre + " a El Remanso";
 
@@ -24,4 +24,73 @@ let huesped;
 for (let huesped = 1; huesped <=2; huesped++){
     reserva= prompt ("ingrese nombre del huesped");
 }
-    alert("Datos ingresados correctamente");
+    alert("Datos ingresados correctamente");*/
+
+    const multiplicar = (a, b) => {
+    return a * b;
+}
+
+const cobrar = (importe)=>{
+    let pago=0;
+    while (importe > 0 && pago !=importe){
+    pago= Number (prompt (`Ingrese ${importe}`))
+    }
+    alert("Muchas gracias por confiar su descanso a El Remanso");
+}
+
+function confirmarReserva(mensaje, precio) {
+    alert(`Muchas gracias por elegirnos, ${mensaje} El total es ${precio}`);
+    cobrar (precio);
+}
+
+function hacerReserva(reserva, cantidad) {
+    let precio = 0;
+    let mensaje = ""
+
+    switch (reserva) {
+        case 1:
+            precio = 2000;
+            precio = multiplicar(precio, cantidad);
+            break;
+        case 2:
+            precio = 3500;
+            precio = multiplicar(precio, cantidad);
+            break;
+        case 3:
+            precio = 5000;
+            precio = multiplicar(precio, cantidad);
+            break;
+        case 4:
+            precio = 7000;
+            precio = multiplicar(precio, cantidad);
+            break;
+        case 5:
+            precio = 8500;
+            precio = multiplicar(precio, cantidad);
+            break;
+        case 6:
+            precio = 10000;
+            precio = multiplicar(precio, cantidad);
+            break;
+        default:
+            mensaje = "No hay disponibilidad";
+            break;
+    }
+    confirmarReserva(mensaje,precio);
+}
+
+const pedirReserva = () => {
+    const reserva = Number(prompt(`Bienvenidos a El Remanso
+    Elija su habitación
+    1- Habitación Simple, 2000
+    2- Habitación Doble, 3500
+    3- Cabaña 4 personas, 5000
+    4- Cabaña 6 personas, 7000
+    5- Cabaña 6 personas, 8500
+    6- Cabaña 10 personas, 10000`));
+    const cantidad = Number(prompt("Ingrese cantidad de habitaciones"));
+    hacerReserva(reserva, cantidad);
+}
+
+
+pedirReserva();
